9d62cfa5b0e9b521feb823515758de7faac84276
timneam <timothyneam@gmail.com>
2022-03-31 21:24:10 +0800

    Started a single page portfolio

---
+++ single-page-portfolio/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import About from './components/About';
+import Contact from './components/Contact';
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </main>
+  );
+}
