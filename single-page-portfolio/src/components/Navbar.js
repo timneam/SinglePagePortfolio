b1d2a3fea6231f9bb5e7aab71659d5f8886ac974
timneam <timothyneam@gmail.com>
2022-04-02 11:17:13 +0800

    Designed Nav bar halfway

--- single-page-portfolio/src/components/Navbar.js
+++ single-page-portfolio/src/components/Navbar.js
@@ -1,15 +1,13 @@
 import React from "react";
+import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <header className="bg-gray-800 md:sticky top-0 z-10">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
+    <header className="nav-bar">
+        <nav className="nav-items">
+        <a href="#about">
             Timothy Neam
           </a>
-        </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#projects">
             Projects
           </a>
@@ -20,7 +18,6 @@ export default function Navbar() {
             Contact
           </a>
         </nav>
-      </div>
     </header>
   );
 }
