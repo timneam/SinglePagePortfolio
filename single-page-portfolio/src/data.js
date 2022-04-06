5bfc434a61811be05c8ddec16672dc6a65a7c2ee
Tim Neam <timothyneam@gmail.com>
2022-04-06 21:37:15 +0800

    Added data file

---
+++ single-page-portfolio/src/data.js
@@ -0,0 +1,178 @@
+export const projects = [
+    {
+        project_name: "Project Panda",
+        description: "Project Panda is a project done for my final year project. It is a hackathorn by Temasek Polytechnic and Singtel. Project Panda is an online ordering system for Singtel canteen for customers",
+        date: "Date: 28 Dec 2021 to 19 Feb 2022",
+        github: "https://github.com/timneam/ProjectPanda",
+        youtube: "https://youtu.be/HJfC6TdpG7U",
+    },
+    {
+        project_name: "Timputer",
+        description: "Timputer was develop during the start of COVID-19 pandemic. Timputer is a computer website where customers can order computer parts or customise their computers.",
+        date: "Date: April 2020 to Sept 2020",
+        github: "https://github.com/timneam/timputer",
+        youtube: "https://youtu.be/GJbG0OvPDvE",
+    },
+    {
+        project_name: "Timozeats",
+        description: "Timozeats is an online ordering food website where customers can order food and check the status of their order.",
+        date: "Date: Sept 2020 to March 2021",
+        github: "https://github.com/timneam/Timozeats---Ionic-Angular",
+        youtube: "https://www.youtube.com/watch?v=D9LTH192Ogs",
+    },
+    {
+        project_name: "Cookphone - Ionic Angular",
+        description: "Cookphone is a project done at the start of the COVID-19 pandemic. Since the pandemic started, many people rushed to the supermarket to buy food. They may not keep track of the ingredients that they have and may have food waste. Cookphone helps users to keep track of the ingredients they have at home.",
+        date: "Date: April 2020 to Sept 2020",
+        github: "https://github.com/timneam/Cookphone",
+        youtube: " ",
+    },
+    {
+        project_name: "Cookphone - Java with Android Studio",
+        description: "Cookphone is a project done at the start of the COVID-19 pandemic. Since the pandemic started, many people rushed to the supermarket to buy food. They may not keep track of the ingredients that they have and may have food waste. Cookphone helps users to keep track of the ingredients they have at home.",
+        date: "Date: April 2020 to Sept 2020",
+        github: "https://github.com/timneam/cookphone-androidstudio",
+        youtube: " ",
+    },
+    {
+        project_name: "TimtheTamManReviews",
+        description: "Tim the Tam Man reviews is a restaurant review website done during my first year in polytechnic. It allows users to view restaurants near their location as well as to view comments from other users.",
+        date: "Date: Sept 2019 to March 2020",
+        github: "https://github.com/timneam/timthetammanreview",
+        youtube: "https://youtu.be/LYxNFvhv1FI",
+    },
+    {
+        project_name: "Music Player",
+        description: "his music player is done during my first year of study. It is a mobile application to allow users to play songs from their devices.",
+        date: "Date: April 2019 to Sept 2019",
+        github: "https://github.com/timneam/Music-Application",
+        youtube: " ",
+    },
+    {
+        project_name: "AI Project: Trash detection in lift",
+        description: "Done during the second year of study where the camera will detect trash in the lift. Once detected, it will send a tweet together with a message to remind the residents to keep the lift clean.",
+        date: "Date: Sept 2020 to Dec 2020",
+        github: "https://github.com/timneam/AI-Project-Trash-Detection-In-Lift",
+        youtube: " ",
+    },
+    {
+        project_name: "Machine Learning",
+        description: "This project was an introduction to machine learning using jupyter notebook. I have created a model to measure the risk of getting stroke with the stroke patients dataset.",
+        date: "Date: Jan 2021 to March 2021",
+        github: "https://github.com/timneam/Machine-Learning",
+        youtube: " ",
+    },
+]
+
+export const coding_skills = [
+    {
+        Name: "Typescript",
+        icon: " "
+    },
+    {
+        Name: "Javascript",
+        icon: " "
+    },
+    {
+        Name: "Ionic",
+        icon: " "
+    },
+    {
+        Name: "Angular",
+        icon: " "
+    },
+    {
+        Name: "Java",
+        icon: " "
+    },
+    {
+        Name: "Python",
+        icon: " "
+    },
+    {
+        Name: "SQL",
+        icon: " "
+    },
+    {
+        Name: "React",
+        icon: " "
+    },
+    {
+        Name: "HTML",
+        icon: " "
+    },
+    {
+        Name: "CSS",
+        icon: " "
+    },
+    {
+        Name: "Angular JS",
+        icon: " "
+    },
+    {
+        Name: "Bootstrap",
+        icon: " "
+    },
+    {
+        Name: "PHP",
+        icon: " "
+    },
+]
+
+export const software = [
+    {
+        Name: "Github",
+        icon: " "
+    },    
+    {
+        Name: "MySQL",
+        icon: " "
+    },    
+    {
+        Name: "Power Bi",
+        icon: " "
+    },    
+    {
+        Name: "Tableau",
+        icon: " "
+    },
+    {
+        Name: "Android Studio",
+        icon: " "
+    },    
+    {
+        Name: "PHP My Admin",
+        icon: " "
+    },    
+    {
+        Name: "Wordpress",
+        icon: " "
+    },    
+    {
+        Name: "Visual Studio Code",
+        icon: " "
+    },
+    {
+        Name: "Visual Studio 2019",
+        icon: " "
+    },    
+    {
+        Name: "Pycharm",
+        icon: " "
+    },
+    {
+        Name: "Anaconda - Jupyter Notebook",
+        icon: " "
+    },
+]
+
+export const database_skills = [
+    {
+        Name: "Google Firebase",
+        icon: " "
+    },
+    {
+        Name: "MongoDB",
+        icon: " "
+    },
+]
