25f19a92c005ae104649fd5bb2be46818e077f82
Tim Neam <timothyneam@gmail.com>
2022-04-11 22:13:38 +0800

    Did some changes to about page

--- single-page-portfolio/src/components/About.js
+++ single-page-portfolio/src/components/About.js
@@ -4,9 +4,38 @@ import "./About.css";
 export default function About() {
     return (
         <div id="about" className="about-container">
-            <h1>
-                About me
-            </h1>
+            <div className="intro-container">
+                <h1>
+                    Hi! My name is Timothy! 
+                </h1>
+            </div>
+            <div className="intro-container2">
+                Currently waiting for university admission!
+            </div>
+            <div className="intro-container3">
+                <ul className="intro-list">
+                    <h1>
+                        Hobbies
+                    </h1>
+                    <li>
+                        Playing games
+                    </li>
+                    <li>
+                        Watching shows
+                    </li>
+                </ul>
+                <ul className="intro-list">
+                    <h1>
+                        Goals
+                    </h1>
+                    <li>
+                        To be a good programmer
+                    </li>
+                    <li>
+                        To work on my weaknesses
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
