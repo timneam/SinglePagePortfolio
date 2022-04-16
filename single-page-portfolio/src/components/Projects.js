b3fb9014032434bec7d69ec9a299f503a1eaca9d
timneam <timothyneam@gmail.com>
2022-04-16 18:04:49 +0800

    Able to make card

--- single-page-portfolio/src/components/Projects.js
+++ single-page-portfolio/src/components/Projects.js
@@ -17,27 +17,31 @@ export default function Projects () {
             {projects.map((project) => {
                 return (
                 <div className="card-container">
-                    <h2>
-                        {project.project_name}
-                    </h2>
-                    {project.description}
-                    <br/>
-                    <br/>
-                    {project.date}
-                    <br/>
-                    <br/>
-                    <div className="buttons">
-                        <button className="button-design">
-                            <a href={project.github} target="_blank" className="button-a">
-                                Github
-                            </a>
-                        </button>
-                        <button className="button-design">
-                            <a href={project.youtube} target="_blank" className="button-a">
-                                Demo
-                            </a>
-                        </button>
-                    </div>
+                    <Card>
+                        <CardContent>
+                            <h2>
+                                {project.project_name}
+                            </h2>
+                            {project.description}
+                            <br/>
+                            <br/>
+                            {project.date}
+                            <br/>
+                            <br/>
+                            <div className="buttons">
+                                <button className="button-design">
+                                    <a href={project.github} target="_blank" className="button-a">
+                                        Github
+                                    </a>
+                                </button>
+                                <button className="button-design">
+                                    <a href={project.youtube} target="_blank" className="button-a">
+                                        Demo
+                                    </a>
+                                </button>
+                            </div> 
+                        </CardContent>
+                </Card>   
                 </div>
                 )
             })}
